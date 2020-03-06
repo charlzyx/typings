cd66823317798b06235edad889e578788055f41e
xiaochao.yang <xiaochao.yang@relxtech.com>
2020-03-06 16:28:53 +0800

    doc: update demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # typings
-> fork this (slate-plugins-next)[https://github.com/zbeyens/slate-plugins-next/tree/master/packages/slate-plugins]
+> fork this [slate-plugins-next](https://github.com/zbeyens/slate-plugins-next/tree/master/packages/slate-plugins)
 
 ## with Image Paste Uploading...
+
+![](https://gitee.com/charlzyx/picgo/raw/master/go/20200306162338.jpg)
--- src/withUploading.tsx
+++ src/withUploading.tsx
@@ -6,10 +6,10 @@ const uploader: Uploader = (file) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (Math.random() > 0.3) {
-        resolve('https://cdn.presslabs.com/wp-content/uploads/2019/03/best-wordpress-photo-gallery-plugins-in-2019.png')
+        resolve('https://i.loli.net/2020/03/06/3MfbV9SzAHoCUXP.jpg')
       } else {
         // fallback as reject
-        reject('https://cdn.presslabs.com/wp-content/uploads/2018/10/upload-error-880x462.png')
+        reject('https://i.loli.net/2020/03/06/tbm5lZSc2d8uYE1.jpg')
       }
     }, 3000);
   });
